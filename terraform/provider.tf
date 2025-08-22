922e84f3cba7379a01f66058cba1d90d89f4aea0
huangdelta <ahuang@deltaops.com.br>
2025-08-22 06:58:12 +0000

    lalala

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.12.2"
+  required_version = "~> 1.11.4"
 
   required_providers {
     aws = {
@@ -10,11 +10,18 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket       = "aws-s3-tfstate-huangfiap"
+    key          = "aws-virtual-machine.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+
 }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
-}
+# provider "aws" {
+#   region                   = "us-east-1"
+#   shared_config_files      = [".aws/config"]
+#   shared_credentials_files = [".aws/credentials"]
+#   profile                  = "iac"
+# }
